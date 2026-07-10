f39966a10e376fc5c11a5e537c9c34e10469866a
Rackoon <32634987+WildRackoon@users.noreply.github.com>
2026-07-10 16:51:32 +0200

    Factor down

--- server_client/main.py
+++ server_client/main.py
@@ -18,7 +18,7 @@ import functools
 
 # CONFIG
 BASE_URL="http://192.168.1.13:6969"
-DRY_RUN=True
+DRY_RUN=False
 
 # IAMGE UTILS
 
@@ -35,6 +35,26 @@ def pre_process_image(img, target_size=512):
         new_height = int(height * scale_factor)
         img = img.resize((new_width, new_height), PIL.Image.Resampling.LANCZOS)
     return img
+
+
+def mosaic(input_img, grid_size: int):
+    if grid_size == 1:
+        return input_img
+    
+    mosaic = PIL.Image.new(input_img.mode, input_img.size)
+
+    width, height = input_img.size
+    tile_width, tile_height = width // grid_size, height // grid_size
+    tile = input_img.resize((tile_width, tile_height), PIL.Image.Resampling.LANCZOS)
+
+    for row in range(grid_size):
+        for col in range(grid_size):
+            # Calculate coordinates based on the smaller tile size
+            x = col * tile_width
+            y = row * tile_height
+            mosaic.paste(tile, (x, y))
+
+    return mosaic
 
 # REQUEST
 def send_request(url, json=None, data=None, method="POST", files=None):
@@ -77,50 +97,49 @@ def func_clear_input(number:int):
     return functools.partial(_clear_inputs, number=1)
 
 
+
+def pil_to_array(img: PIL.Image.Image):
+    byte_arr = io.BytesIO()
+    img.save(byte_arr, format='PNG')
+    byte_arr.seek(0)
+    return byte_arr
+
 # GRADIO
-def process_image(input_img: PIL.Image.Image, size):
+def process_image(input_img: PIL.Image.Image):
     if input_img is None:
         return "Please upload an image first!"
 
-
-    # Rotate and scale 512xH
-    input_img = pre_process_image(input_img)
+    # TODO ONLY WHEN USING ACTUAL INPUT, MAYBE DO A CHECK HERE ANYWAY
+    # input_img = pre_process_image(input_img)
     # width, height = input_img.size
-    # mosaic = PIL.Image.Image(input_img.mode, input_img.size)
 
-
-    factor_map={
-        i: 1.0/float(i)
-        for i in (1, 2, 3)
-    }
-    factor=factor_map[int(size)]
-    rich.print(f"Must print {factor}")
-
-    # Example logic based on the selected size
-    # width, height = input_img.size
-    # TODO FACTORS    
-
-
-    # 1. Convert the PIL Image into bytes in memory
-    byte_arr = io.BytesIO()
-    input_img.save(byte_arr, format='PNG')
-    byte_arr.seek(0)
+    # Convert the PIL Image into bytes in memory
+    img_arr = pil_to_array(input_img)
 
     res = send_request(
         f"{BASE_URL}/api/image",
         data={"metadata": "dummyshit"},
         files={
-            'image': ('image.png', byte_arr, 'image/png')  # TODO NAME
+            'image': ('image.png', img_arr, 'image/png')  # TODO NAME
         }
     )
     # rich.print(res.json())
     gr.Success("Success")
 
 def process_image_change(image_input, *args):
+    grid_size_str = args[0]
+    grid_size=int(grid_size_str)
+
     if image_input is None:
         return "", None
-    rich.print(args)
-    return f"Size: {image_input.size}", image_input
+
+    # Rotate and scale 512xH
+    out_image = pre_process_image(image_input)
+
+    if grid_size != 1:
+        out_image = mosaic(out_image, grid_size)
+
+    return f"Size: {image_input.size}", out_image
 
 
 
@@ -171,17 +190,26 @@ def main():
         # with gr.Row():
         input_widgets = {
             "size_selector": gr.Radio(
-                choices=["1", "2", "3"], 
-                value="1", 
+                choices=[1, 2, 3], 
+                value=1, 
                 label="Select Output Size"
             )
         }
 
+        # Changes to Image or inputs
+        preview_inputs_list = [input_img, *input_widgets.values()]
+        preview_outputs_list = [input_status, img_preview]
         input_img.change(
             fn=process_image_change,
-            inputs=[input_img, *input_widgets.values()],
-            outputs=[input_status, img_preview] # COULD AUTO CHOOSE size_selector
+            inputs=preview_inputs_list,
+            outputs=preview_outputs_list
         )
+        for input_widget in input_widgets.values():
+                input_widget.change(
+                    fn=process_image_change,
+                    inputs=preview_inputs_list,
+                    outputs=preview_outputs_list
+                )
 
         # Send Button
         send_btn = gr.Button("Send", variant="primary")
@@ -190,7 +218,8 @@ def main():
         # 3. Define the click event logic
         send_btn.click(
             fn=process_image,
-            inputs=[input_img, *input_widgets.values()],  # TODO can be a set => means it will give us a **kwargs dict in return
+            # inputs=[input_img, *input_widgets.values()],  # TODO can be a set => means it will give us a **kwargs dict in return
+            inputs=[img_preview],  # TODO can be a set => means it will give us a **kwargs dict in return
         ).success(
             func_clear_input(1),
             inputs=None,
